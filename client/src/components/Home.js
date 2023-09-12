362d4c5b61ef0058c6ae7b7e46523300d7759ea0
kevinnguyen20 <kk.nguyen@web.de>
2023-09-12 19:46:11 +0200

    Rename tags, add class names

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,35 +11,31 @@ const Home = () => {
         if (audioElement && canvasElement) {
             const wave = new Wave(audioElement, canvasElement);
             wave.addAnimation(new wave.animations.Lines({
-                // lineWidth: 10,
-                // lineColor: "red",
                 count: 30,
                 lineWidth: 1,
                 top: 1,
                 lineColor: "white"
             }));
-        }
 
-        if (audioElement) {
-            audioElement.volume = 0.1;
-          }
+            audioElement.volume = 0.2;
+        };
       }, []);
 
-    return (
-        <div className="home">
-            <h1>Welcome, traveler!</h1>
-            <p>
-                <span>Emerge in a world full of limitless possibilities, where creativity knows no bounds.</span>
-                <span>Welcome to our digital haven, a space crafted by us, for us.</span>
-                <span>Explore, create, and inspire with us on this exciting journey.</span>
-            </p>
-            <canvas id="canvas"></canvas>
+      return (
+        <div className="homeContainer">
+            <h1 className="homeHeader">Welcome, traveler!</h1>
+            <div className="homeText">
+                <p className="homeSen1">Emerge in a world full of limitless possibilities, where creativity knows no bounds.</p>
+                <p className="homeSen2">Welcome to our digital haven, a space crafted by us, for us.</p>
+                <p className="homeSen3">Explore, create, and inspire with us on this exciting journey.</p>
+            </div>
+            <canvas id="canvas" className="homeCanvas"></canvas>
             <div id="audio"></div>
             <audio autoPlay loop>
                 {/* <source src="audio/in-the-forest-ambient-acoustic-guitar-instrumental-background-music-for-videos-5718.mp3" type="audio/mpeg" /> */}
                 {/* <source src="audio/the-introvert-michael-kobrin-10959.mp3" type="audio/mpeg" /> */}
                 {/* <source src="audio/smile-relaxing-cinematic-background-piano-music-162112.mp3" type="audio/mpeg" /> */}
-                <source src="audio/soft-dreamy-ambient-113221.mp3" type="audio/mpeg" />
+                <source src="/audio/soft-dreamy-ambient-113221.mp3" type="audio/mpeg" />
                 Your browser does not support the audio element.
             </audio>
         </div>
